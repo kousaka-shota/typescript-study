9ead283644f99e4854417e8882b2e6cd0f416f49
Shota Kousaka <114033759+kousaka-shota@users.noreply.github.com>
2023-01-18 08:53:46 +0900

    Initial commit

---
+++ src/typeList.ts
@@ -0,0 +1,18 @@
+//boolean
+let bool: boolean = true;
+
+//number
+let num: number = 0;
+
+//string
+let str: string = "A";
+
+//array
+let arr1: Array<number> = [0, 1, 2];
+let arr2: number[] = [0, 1, 2];
+
+//tuple
+let tuple: [number, string] = [0, "A"];
+
+//any
+let any: any = false;
