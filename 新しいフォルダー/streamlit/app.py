35c2619dd35496ad3502786dc6ac546db10acb1a
Shota Kousaka <114033759+kousaka-shota@users.noreply.github.com>
2023-07-13 07:55:45 +0900

    Delete 新しいフォルダー/streamlit directory

--- 新しいフォルダー/streamlit/app.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-
-options = st.multiselect(
-    'What are your favorite colors',
-    ['Green', 'Yellow', 'Red', 'Blue'],
-    ['Yellow', 'Red'],)
-
-st.write('You selected:', options)
