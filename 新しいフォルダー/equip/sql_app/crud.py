c5eb4d71cdf2b4caeec1c7728275af6da971ce15
Shota Kousaka <114033759+kousaka-shota@users.noreply.github.com>
2023-07-14 06:33:01 +0900

    Add files via upload

--- 新しいフォルダー/equip/sql_app/crud.py
+++ 新しいフォルダー/equip/sql_app/crud.py
@@ -14,7 +14,18 @@ def get_sensors(db:Session):
 #計測データをフィルターして取得（期間・設備・センサ―）
 
 #設備の追加
+def add_equipment(db:Session,equipment:schemas.Equipment):
+    #API側から取得している登録したい情報をインスタンスを生成時の引数として渡す。id等の自動登録されるものは渡さない
+    db_equipment = models.Equipment(equip_name=equipment.equip_name,equip_asset_No=equipment.equip_asset_No)
 
+    #dbへ仮保存。複数データを登録する場合はaddをたくさんしてからまとめて一回でcomitする
+    db.add(db_equipment)
+    db.commit()
+
+    #commitでdb側に反映されるがセッション情報をリセットし新しい情報を入れるために必要
+    db.refresh(db_equipment)
+    return db_equipment
+    
 #センサ―の追加
 
 #データの追加
